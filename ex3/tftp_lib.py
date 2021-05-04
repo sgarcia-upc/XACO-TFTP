5c4213de94de1cc5ede4194ca6003815176e32fe
Sergi <sgarcia030@gmail.com>
2021-05-04 02:42:41 +0200

    ack are now correct and data too

--- ex3/tftp_lib.py
+++ ex3/tftp_lib.py
@@ -11,63 +11,59 @@ def generateTID():
 def send_file(socket, server, port, filename, size, modo):
     try:
         if modo=="netascii":
-            f = open(filename, "r")
+            m = "r"
         elif modo=="octet":
-            f = open(filename, "rb")
+            m = "rb"
         else:
             print("NoSuchMODO")
             sys.exit(1)
-        block_num_ack = 0
-        block_num = 1
-        file_data = f.read(size)
-        data = pkg.generate_data(block_num, file_data)
-        print("sending DATA: %s -- %s"%(block_num, len(file_data)))        
-        # TODO: if file is empty
-        while (len(file_data) > 0):
-            block_num = block_num % 65535
-            if(block_num == 0):
-                block_num = 1
-            if (socket.sendto(data, (server, port))):
-                # ~ ack, add = socket.recvfrom(4)
-                # ~ #TODO: check op code
-                # ~ block_num_ack = pkg.decodificate_ack(ack)
-                # ~ print("receiving ACK: %s"%(block_num_ack))
+
+        with open(filename, "rb") as f:
+            block_num_ack = 0
+            block_num = 65534
+            file_data = f.read(size)
+            data = pkg.generate_data(block_num, file_data)
+            # TODO: if file is empty
+            while (len(file_data) > 0):
+
                 while(block_num != block_num_ack):
                     socket.sendto(data, (server, port))
+                    print("sending DATA: %s -- %s"%(block_num, len(file_data)))        
                     ack, add = socket.recvfrom(4)
                     block_num_ack =  pkg.decodificate_ack(ack)
                     print("receiving ACK: %s"%(block_num_ack))
+
                 block_num+=1
+                block_num = block_num % 65535
+                if(block_num == 0):
+                    block_num = 1
+
                 if(len(file_data) == size):
                     file_data = f.read(size)
                     data = pkg.generate_data(block_num, file_data)
-                    print("receiving ACK: %s"%(block_num_ack))
                     if (len(file_data) == 0): 
-                        socket.sendto(data, (server, port))
                         while(block_num != block_num_ack):
                             socket.sendto(data, (server, port))
+                            print("sending DATA: %s -- %s"%(block_num, len(file_data)))
                             ack, add = socket.recvfrom(4)
                             block_num_ack =  pkg.decodificate_ack(ack)
                             print("receiving ACK: %s"%(block_num_ack))
                         block_num+=1
+                        block_num = block_num % 65535
+                        if(block_num == 0):
+                            block_num = 1
                 else:
                     file_data = bytes()
-                    while(block_num != block_num_ack):
-                            socket.sendto(data, (server, port))
-                            ack, add = socket.recvfrom(4)
-                            block_num_ack =  pkg.decodificate_ack(ack)
-                            print("receiving ACK: %s"%(block_num_ack))
+                    #while(block_num != block_num_ack):
+                    #        socket.sendto(data, (server, port))
+                    #        ack, add = socket.recvfrom(4)
+                    #        block_num_ack =  pkg.decodificate_ack(ack)
+                    #        print("receiving ACK: %s"%(block_num_ack))
                     
-            print("sending DATA: %s -- %s"%(block_num, len(file_data)))
             
     except IOError as e:
         print("File requested not found")
         print(e)
-    finally:
-        try:
-            f.close()
-        except:
-            pass
 
 def recv_file(socket, server, port, filename, size, modo):
     """
@@ -98,16 +94,13 @@ def recv_file(socket, server, port, filename, size, modo):
                 data, addr = socket.recvfrom(4+size)
                 #TODO: mirar si el data tiene op_code data...
                 num_block, file_data = pkg.decodificate_data(data)
+                print("receiving DATA: %s -- %s"%(num_block, len(file_data)))
                 ack = pkg.generate_ack(num_block)
                 socket.sendto(ack, (server, port))
                 print("sending ACK: %s"%(num_block))
             else:
                 file_data = bytes()
-                ack = pkg.generate_ack(num_block)
-                socket.sendto(ack, (server, port))
-                print("sending ACK: %s"%(num_block))
             
-            print("receiving DATA: %s -- %s"%(num_block, len(file_data)))
             
     except IOError:
         print("File requested not found")
